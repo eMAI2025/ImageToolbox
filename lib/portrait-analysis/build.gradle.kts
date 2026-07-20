24c5066b811d7267845fb45e5cf7ff792c9d9082
eMAI2025 <248235758+eMAI2025@users.noreply.github.com>
2026-07-20 07:29:05 +0200

    Add portrait analysis library module scaffold

---
+++ lib/portrait-analysis/build.gradle.kts
@@ -0,0 +1,26 @@
+/*
+ * ImageToolbox is an image editor for android
+ * Copyright (c) 2026 T8RIN (Malik Mukhametzyanov)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+plugins {
+    alias(libs.plugins.image.toolbox.library)
+}
+
+android.namespace = "com.t8rin.imagetoolbox.lib.portrait_analysis"
+
+dependencies {
+    testImplementation(libs.junit)
+}
